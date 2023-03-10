d5b61733d6759c4600384fe5e1d028d16a587478
Dboee <Davidboee@gmail.com>
2023-03-10 09:33:41 +0100

    Initial project setup from ticket template

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// starts the express server
+// and connecting to the database.
+
+//DB
+
+// Listeners
+
+const start = async () => {
+  console.log('Expiration Service starting up...');
+};
+
+start();
